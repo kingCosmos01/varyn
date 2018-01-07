cacf1a28ae9c3e6a782df8d595b9ad005d601c62
John Foster <john@varyn.com>
2018-01-07 14:12:07 -0800

    Sync commonUtilities.js

--- public/common/commonUtilities.js
+++ public/common/commonUtilities.js
@@ -16,7 +16,7 @@
     'use strict';
 
     var commonUtilities = {
-        version: '1.2.8'
+        version: "1.2.9"
     },
     _base64KeyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
     _testNumber = 0;
@@ -340,7 +340,7 @@
      *
      */
     commonUtilities.isMobile = function () {
-        return (commonUtilities.isMobileAndroid() || commonUtilities.isMobileBlackberry() || commonUtilities.isMobileIos() || commonUtilities.isMobileWindows());
+        return (this.isMobileAndroid() || this.isMobileBlackberry() || this.isMobileIos() || this.isMobileWindows());
     };
 
     commonUtilities.isMobileAndroid = function () {
@@ -469,7 +469,7 @@
             chr1, chr2, chr3, enc1, enc2, enc3, enc4,
             i = 0;
 
-        input = commonUtilities.utf8Encode(input);
+        input = this.utf8Encode(input);
         while (i < inputLength) {
             chr1 = input.charCodeAt(i ++);
             chr2 = input.charCodeAt(i ++);
@@ -520,7 +520,7 @@
                 output = output + String.fromCharCode(chr3);
             }
         }
-        return commonUtilities.utf8Decode(output);
+        return this.utf8Decode(output);
     };
 
     /**
@@ -599,7 +599,7 @@
      * @return {boolean} true if removed, false if doesn't exist.
      */
     commonUtilities.cookieRemove = function (key, path, domain) {
-        if (commonUtilities.cookieExists(key)) {
+        if (this.cookieExists(key)) {
             document.cookie = encodeURIComponent(key) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" + (domain ? "; domain=" + domain : "") + (path ? "; path=" + path : "");
             return true;
         } else {
@@ -920,7 +920,7 @@
             }
             tagList = tags.split(delimiter);
             for (i = tagList.length - 1; i >= 0; i --) {
-                tagList[i] = commonUtilities.strip_tags(tagList[i].trim(), '').substr(0, 50).trim();
+                tagList[i] = this.stripTags(tagList[i].trim(), '').substr(0, 50).trim();
                 if (tagList[i].length < 2) {
                     tagList.splice(i, 1);
                 }
@@ -938,13 +938,22 @@
      * @param allowed {string} list of tags to accept.
      * @returns {string} the stripped result.
      */
-    commonUtilities.strip_tags = function (input, allowed) {
+    commonUtilities.stripTags = function (input, allowed) {
         allowed = (((allowed || '') + '').toLowerCase().match(/<[a-z][a-z0-9]*>/g) || []).join('');
         var tags = /<\/?([a-z][a-z0-9]*)\b[^>]*>/gi;
         var commentsAndPhpTags = /<!--[\s\S]*?-->|<\?(?:php)?[\s\S]*?\?>/gi;
         return input.replace(commentsAndPhpTags, '').replace(tags, function ($0, $1) {
             return allowed.indexOf('<' + $1.toLowerCase() + '>') > -1 ? $0 : ''
         });
+    };
+
+    /**
+     * Determine if a string looks like a valid email address.
+     * @param email {string}
+     * @returns {boolean}
+     */
+    commonUtilities.isValidEmail = function(email) {
+        return /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email);
     };
 
     /**
@@ -968,7 +977,7 @@
      *   set of rules. The rule set itself is defined as a key/value array of mandatory and
      *   optional keys, as follows:
      *   type: string defining the data type expected. Optional, the default is "string".
-     *         Valid types are string, number, bool, array, date.
+     *         Valid types are string, number, bool, array, date, email.
      *   optional: boolean indicates if the field value is optional. When true, the key
      *         does not have to exist in keyValueArrayOfFields. If it does exist we accept
      *         no value for the field (null, "", or any valid empty value.) If it does
@@ -1052,6 +1061,7 @@
                                 }
                                 break;
                             case "bool":
+                            case "boolean":
                                 if (options.length > 0) {
                                     if (options.indexOf(fieldValue) < 0) {
                                         result[field] = {code: "options", message: "The field value is not an option."};
@@ -1067,7 +1077,7 @@
                                         result[field] = {code: "max", message: "The date field is after the maximum date allowed."};
                                     } else if (options.length > 0) {
                                         if (options.indexOf(fieldTime) < 0) {
-                                            result[field] = {code: "options", message: "The field value is not an option."};
+                                            result[field] = {code: "options", message: "The field value is not a valid option."};
                                         }
                                     }
                                 }
@@ -1080,10 +1090,15 @@
                                 } else if (options.length > 0) {
                                     for (i = 0; i < fieldValue.length; i ++) {
                                         if (options.indexOf(fieldValue[i]) < 0) {
-                                            result[field] = {code: "options", message: "A field value is not an option."};
+                                            result[field] = {code: "options", message: "A field value is not a valid option."};
                                             break;
                                         }
                                     }
+                                }
+                                break;
+                            case "email":
+                                if ( ! this.isEmpty(fieldValue) && ! this.isValidEmail(fieldValue)) {
+                                    result[field] = {code: "invalid", message: "The email address is not valid."};
                                 }
                                 break;
                         }
@@ -1106,14 +1121,14 @@
         function K(G,k) {
             var I,d,F,H,x;
             F=(G&2147483648);H=(k&2147483648);I=(G&1073741824);d=(k&1073741824);x=(G&1073741823)+(k&1073741823);
-            if(I&d){return(x^2147483648^F^H)}
-            if(I|d){if(x&1073741824){return(x^3221225472^F^H)}else{return(x^1073741824^F^H)}}else{return(x^F^H)}
+            if(I&d){return(x^2147483648^F^H);}
+            if(I|d){if(x&1073741824){return(x^3221225472^F^H);}else{return(x^1073741824^F^H);}}else{return(x^F^H);}
         }
         function r(d,F,k){
-            return(d&F)|((~d)&k)
+            return(d&F)|((~d)&k);
         }
         function q(d,F,k){
-            return(d&k)|(F&(~k))
+            return(d&k)|(F&(~k));
         }
         function p(d,F,k){return(d^F^k)}
         function n(d,F,k){return(F^(d|(~k)))}
@@ -1124,38 +1139,38 @@
         function e(G){
             var Z;var F=G.length;var x=F+8;var k=(x-(x%64))/64;var I=(k+1)*16;var aa=Array(I-1);var d=0;var H=0;
             while(H<F){
-                Z=(H-(H%4))/4;d=(H%4)*8;aa[Z]=(aa[Z]|(G.charCodeAt(H)<<d));H++
+                Z=(H-(H%4))/4;d=(H%4)*8;aa[Z]=(aa[Z]|(G.charCodeAt(H)<<d));H++;
             }
             Z=(H-(H%4))/4;d=(H%4)*8;aa[Z]=aa[Z]|(128<<d);aa[I-2]=F<<3;aa[I-1]=F>>>29;
-            return aa
+            return aa;
         }
         function B(x){
             var k="",F="",G,d;
             for(d=0;d<=3;d++){
-                G=(x>>>(d*8))&255;F="0"+G.toString(16);k=k+F.substr(F.length-2,2)
+                G=(x>>>(d*8))&255;F="0"+G.toString(16);k=k+F.substr(F.length-2,2);
             }
-            return k
+            return k;
         }
         function J(k){
             k=k.replace(/rn/g,"n");var d="";
             for(var F=0;F<k.length;F++){
                 var x=k.charCodeAt(F);
                 if(x<128){
-                    d+=String.fromCharCode(x)
+                    d+=String.fromCharCode(x);
                 }else{
                     if((x>127)&&(x<2048)){
-                        d+=String.fromCharCode((x>>6)|192);d+=String.fromCharCode((x&63)|128)
+                        d+=String.fromCharCode((x>>6)|192);d+=String.fromCharCode((x&63)|128);
                     }else{
-                        d+=String.fromCharCode((x>>12)|224);d+=String.fromCharCode(((x>>6)&63)|128);d+=String.fromCharCode((x&63)|128)
+                        d+=String.fromCharCode((x>>12)|224);d+=String.fromCharCode(((x>>6)&63)|128);d+=String.fromCharCode((x&63)|128);
                     }
                 }
             }
-            return d
+            return d;
         }
-        var C=Array();var P,h,E,v,g,Y,X,W,V;var S=7,Q=12,N=17,M=22;var A=5,z=9,y=14,w=20;var o=4,m=11,l=16,j=23;var U=6,T=10,R=15,O=21;
+        var C;var P,h,E,v,g,Y,X,W,V;var S=7,Q=12,N=17,M=22;var A=5,z=9,y=14,w=20;var o=4,m=11,l=16,j=23;var U=6,T=10,R=15,O=21;
         s=J(s);C=e(s);Y=1732584193;X=4023233417;W=2562383102;V=271733878;
         for(P=0;P<C.length;P+=16){
-            h=Y;E=X;v=W;g=V;Y=u(Y,X,W,V,C[P+0],S,3614090360);V=u(V,Y,X,W,C[P+1],Q,3905402710);W=u(W,V,Y,X,C[P+2],N,606105819);X=u(X,W,V,Y,C[P+3],M,3250441966);Y=u(Y,X,W,V,C[P+4],S,4118548399);V=u(V,Y,X,W,C[P+5],Q,1200080426);W=u(W,V,Y,X,C[P+6],N,2821735955);X=u(X,W,V,Y,C[P+7],M,4249261313);Y=u(Y,X,W,V,C[P+8],S,1770035416);V=u(V,Y,X,W,C[P+9],Q,2336552879);W=u(W,V,Y,X,C[P+10],N,4294925233);X=u(X,W,V,Y,C[P+11],M,2304563134);Y=u(Y,X,W,V,C[P+12],S,1804603682);V=u(V,Y,X,W,C[P+13],Q,4254626195);W=u(W,V,Y,X,C[P+14],N,2792965006);X=u(X,W,V,Y,C[P+15],M,1236535329);Y=f(Y,X,W,V,C[P+1],A,4129170786);V=f(V,Y,X,W,C[P+6],z,3225465664);W=f(W,V,Y,X,C[P+11],y,643717713);X=f(X,W,V,Y,C[P+0],w,3921069994);Y=f(Y,X,W,V,C[P+5],A,3593408605);V=f(V,Y,X,W,C[P+10],z,38016083);W=f(W,V,Y,X,C[P+15],y,3634488961);X=f(X,W,V,Y,C[P+4],w,3889429448);Y=f(Y,X,W,V,C[P+9],A,568446438);V=f(V,Y,X,W,C[P+14],z,3275163606);W=f(W,V,Y,X,C[P+3],y,4107603335);X=f(X,W,V,Y,C[P+8],w,1163531501);Y=f(Y,X,W,V,C[P+13],A,2850285829);V=f(V,Y,X,W,C[P+2],z,4243563512);W=f(W,V,Y,X,C[P+7],y,1735328473);X=f(X,W,V,Y,C[P+12],w,2368359562);Y=D(Y,X,W,V,C[P+5],o,4294588738);V=D(V,Y,X,W,C[P+8],m,2272392833);W=D(W,V,Y,X,C[P+11],l,1839030562);X=D(X,W,V,Y,C[P+14],j,4259657740);Y=D(Y,X,W,V,C[P+1],o,2763975236);V=D(V,Y,X,W,C[P+4],m,1272893353);W=D(W,V,Y,X,C[P+7],l,4139469664);X=D(X,W,V,Y,C[P+10],j,3200236656);Y=D(Y,X,W,V,C[P+13],o,681279174);V=D(V,Y,X,W,C[P+0],m,3936430074);W=D(W,V,Y,X,C[P+3],l,3572445317);X=D(X,W,V,Y,C[P+6],j,76029189);Y=D(Y,X,W,V,C[P+9],o,3654602809);V=D(V,Y,X,W,C[P+12],m,3873151461);W=D(W,V,Y,X,C[P+15],l,530742520);X=D(X,W,V,Y,C[P+2],j,3299628645);Y=t(Y,X,W,V,C[P+0],U,4096336452);V=t(V,Y,X,W,C[P+7],T,1126891415);W=t(W,V,Y,X,C[P+14],R,2878612391);X=t(X,W,V,Y,C[P+5],O,4237533241);Y=t(Y,X,W,V,C[P+12],U,1700485571);V=t(V,Y,X,W,C[P+3],T,2399980690);W=t(W,V,Y,X,C[P+10],R,4293915773);X=t(X,W,V,Y,C[P+1],O,2240044497);Y=t(Y,X,W,V,C[P+8],U,1873313359);V=t(V,Y,X,W,C[P+15],T,4264355552);W=t(W,V,Y,X,C[P+6],R,2734768916);X=t(X,W,V,Y,C[P+13],O,1309151649);Y=t(Y,X,W,V,C[P+4],U,4149444226);V=t(V,Y,X,W,C[P+11],T,3174756917);W=t(W,V,Y,X,C[P+2],R,718787259);X=t(X,W,V,Y,C[P+9],O,3951481745);Y=K(Y,h);X=K(X,E);W=K(W,v);V=K(V,g)
+            h=Y;E=X;v=W;g=V;Y=u(Y,X,W,V,C[P+0],S,3614090360);V=u(V,Y,X,W,C[P+1],Q,3905402710);W=u(W,V,Y,X,C[P+2],N,606105819);X=u(X,W,V,Y,C[P+3],M,3250441966);Y=u(Y,X,W,V,C[P+4],S,4118548399);V=u(V,Y,X,W,C[P+5],Q,1200080426);W=u(W,V,Y,X,C[P+6],N,2821735955);X=u(X,W,V,Y,C[P+7],M,4249261313);Y=u(Y,X,W,V,C[P+8],S,1770035416);V=u(V,Y,X,W,C[P+9],Q,2336552879);W=u(W,V,Y,X,C[P+10],N,4294925233);X=u(X,W,V,Y,C[P+11],M,2304563134);Y=u(Y,X,W,V,C[P+12],S,1804603682);V=u(V,Y,X,W,C[P+13],Q,4254626195);W=u(W,V,Y,X,C[P+14],N,2792965006);X=u(X,W,V,Y,C[P+15],M,1236535329);Y=f(Y,X,W,V,C[P+1],A,4129170786);V=f(V,Y,X,W,C[P+6],z,3225465664);W=f(W,V,Y,X,C[P+11],y,643717713);X=f(X,W,V,Y,C[P+0],w,3921069994);Y=f(Y,X,W,V,C[P+5],A,3593408605);V=f(V,Y,X,W,C[P+10],z,38016083);W=f(W,V,Y,X,C[P+15],y,3634488961);X=f(X,W,V,Y,C[P+4],w,3889429448);Y=f(Y,X,W,V,C[P+9],A,568446438);V=f(V,Y,X,W,C[P+14],z,3275163606);W=f(W,V,Y,X,C[P+3],y,4107603335);X=f(X,W,V,Y,C[P+8],w,1163531501);Y=f(Y,X,W,V,C[P+13],A,2850285829);V=f(V,Y,X,W,C[P+2],z,4243563512);W=f(W,V,Y,X,C[P+7],y,1735328473);X=f(X,W,V,Y,C[P+12],w,2368359562);Y=D(Y,X,W,V,C[P+5],o,4294588738);V=D(V,Y,X,W,C[P+8],m,2272392833);W=D(W,V,Y,X,C[P+11],l,1839030562);X=D(X,W,V,Y,C[P+14],j,4259657740);Y=D(Y,X,W,V,C[P+1],o,2763975236);V=D(V,Y,X,W,C[P+4],m,1272893353);W=D(W,V,Y,X,C[P+7],l,4139469664);X=D(X,W,V,Y,C[P+10],j,3200236656);Y=D(Y,X,W,V,C[P+13],o,681279174);V=D(V,Y,X,W,C[P+0],m,3936430074);W=D(W,V,Y,X,C[P+3],l,3572445317);X=D(X,W,V,Y,C[P+6],j,76029189);Y=D(Y,X,W,V,C[P+9],o,3654602809);V=D(V,Y,X,W,C[P+12],m,3873151461);W=D(W,V,Y,X,C[P+15],l,530742520);X=D(X,W,V,Y,C[P+2],j,3299628645);Y=t(Y,X,W,V,C[P+0],U,4096336452);V=t(V,Y,X,W,C[P+7],T,1126891415);W=t(W,V,Y,X,C[P+14],R,2878612391);X=t(X,W,V,Y,C[P+5],O,4237533241);Y=t(Y,X,W,V,C[P+12],U,1700485571);V=t(V,Y,X,W,C[P+3],T,2399980690);W=t(W,V,Y,X,C[P+10],R,4293915773);X=t(X,W,V,Y,C[P+1],O,2240044497);Y=t(Y,X,W,V,C[P+8],U,1873313359);V=t(V,Y,X,W,C[P+15],T,4264355552);W=t(W,V,Y,X,C[P+6],R,2734768916);X=t(X,W,V,Y,C[P+13],O,1309151649);Y=t(Y,X,W,V,C[P+4],U,4149444226);V=t(V,Y,X,W,C[P+11],T,3174756917);W=t(W,V,Y,X,C[P+2],R,718787259);X=t(X,W,V,Y,C[P+9],O,3951481745);Y=K(Y,h);X=K(X,E);W=K(W,v);V=K(V,g);
         }
         var i=B(Y)+B(X)+B(W)+B(V);
         return i.toLowerCase();
@@ -1169,7 +1184,7 @@
      */
     commonUtilities.getGravatarURL = function (email, size) {
         var size = size || 80;
-        return 'https://www.gravatar.com/avatar/' + commonUtilities.MD5(email) + '.jpg?s=' + size;
+        return 'https://www.gravatar.com/avatar/' + this.MD5(email) + '.jpg?s=' + size;
     };
 
     /* ----------------------------------------------------------------------------------
