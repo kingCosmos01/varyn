79841a572ef59ea5a09c5262f02065bdee3e9df7
John Foster <john@varyn.com>
2019-04-07 17:12:54 -0700

    update sitemap builder #253

--- public/sitemap/index.php
+++ public/sitemap/index.php
@@ -18,6 +18,8 @@ include_once(VIEWS_ROOT . 'header.php');
 <div id="section-Varyn">
   <h3>Varyn</h3>
   <ul>
+<li><a href="/">Varyn: Great games you can play anytime, anywhere</a> Varyn makes games using technology that performs on the most popular platforms. Cross platform friendly technologies have created an opportunity to re-invent online games for an audience that moves seamlessly between desktop, tablet, and smart-phone.</li>
+
 <li><a href="/about/">About Varyn</a> Learn more about who is Varyn, our mission, and what we stand for.</li>
 
 <li><a href="/contact/">Contact Varyn</a> Contact us if you have something to say, you need help, or if you are interested in more information about what we do.</li>
@@ -41,6 +43,138 @@ include_once(VIEWS_ROOT . 'header.php');
 <li><a href="/games/">Experimental Games at Varyn.com</a> Check out some of the games that are in development, betas, experiments in new game play ideas, and the research and development going on at Varyn.com.</li>
 
 <li><a href="/profile/">Profile</a> View your player profile or review other followers and players at Varyn.com.</li>
+
+<li><a href="/play/?id=1000">Zam BeeZee</a> Make as many words as you can to fill up the honey barrel before time runs out. Different tiles and letter combos keep the action sweet and addicting.</li>
+
+<li><a href="/play/?id=1001">Capital Collision</a> Shoot your way through 5 levels of arcade action and discover the truth behind the politics.</li>
+
+<li><a href="/play/?id=1002">Classic Mah Jongg</a> Mah jongg is a solitaire game where you are challenged to remove all pieces from the board by matching pairs of unblocked tiles.</li>
+
+<li><a href="/play/?id=1003">Baseball Juiced</a> Choose your personal training method and swing for the record books. Can you make it into the Hall of Fame?</li>
+
+<li><a href="/play/?id=1004">Karoshi Suicide</a> Lost all your money? Fine, now you lose your life! Punish this pawn of industry by pushing him down a path of destruction!</li>
+
+<li><a href="/play/?id=1005">Missile Defence</a> Your cities are in peril! Fortunately, you have powerful laser towers to ward off the alien invasion.</li>
+
+<li><a href="/play/?id=1006">Block Drop</a> Find your way home by jumping from block to block. Sink all the blocks and finish on the checkered flag to win.</li>
+
+<li><a href="/play/?id=1008">Parking Frenzy</a> Cram cars into their parking spaces carefully and crash-free! Precise parking is the name of the game in this parking puzzler.</li>
+
+<li><a href="/play/?id=1009">Ninja Plus</a> Ninja missions! Collect gold, defeat your many enemies, and wreak ninja havoc! When you're done, build your own ninja levels!</li>
+
+<li><a href="/play/?id=1010">Pencil Racer 3</a> Pencil Racer is officially off the hook! Drivable vehicles, hazards, powerups, and collectibles. Make killer tracks and share them with your friends.</li>
+
+<li><a href="/play/?id=1011">DJ Fest 2</a> Hey, DJ! Keep the needle in that phat groove, baby! Demonstrate your scratch skills to score big!</li>
+
+<li><a href="/play/?id=1012">Celebrity Snapshot</a> Make a few bucks by catching celebs in the act. Avoid getting your lights punched out by bodyguards!</li>
+
+<li><a href="/play/?id=1013">Sniper Assassin 3</a> Sir Sniper found his wife's killer but the story only gets more complex. Fortunately, there are bullets for all!</li>
+
+<li><a href="/play/?id=1014">Bubble Tanks 2</a> Travel through giant bubbles, destroy enemy tanks, take their bubbles to fuel your growth.</li>
+
+<li><a href="/play/?id=1016">Gem Craft</a> You are the wizard who can end the havoc and corruption throughout the land. Banish the monsters back to hell with your magic gems and towers!</li>
+
+<li><a href="/play/?id=1017">Vector Boom</a> Your space station against a vast incoming armada of spacecraft. Fortunately you've got an awesome weapon on board.</li>
+
+<li><a href="/play/?id=1018">Money Seize</a> Sir Reginald MoneySeize II, Esq. must collect coins to fund construction of the tallest tower in the world!</li>
+
+<li><a href="/play/?id=1021">The Enginesis Test Game</a> This game will help test any services provided by Enginesis.</li>
+
+<li><a href="/play/?id=1023">Street Fighter II: Champion Edition</a> Select and fight your opponent in martial arts matches around the world. Win your fight and move on to the next stage.</li>
+
+<li><a href="/play/?id=1042">Skywords</a> Find all the words and do it quick to earn a stratospheric score.</li>
+
+<li><a href="/play/?id=1043">Winter of Discontent</a> Battle the North Pole's true indigenous life. Can you stop the Ice King?</li>
+
+<li><a href="/play/?id=1044">Kanye West Glory Hog</a> How many awards can you stack up in the back of your truck? Can you help Kanye grab all the awards and hog your way to glory?</li>
+
+<li><a href="/play/?id=1046">Microbe Muncher</a> Slither around and eat microbes while avoiding everything else.</li>
+
+<li><a href="/play/?id=1047">Morningstar</a> Point 'n click adventure, escape a distant planet after crashing into it's surface.</li>
+
+<li><a href="/play/?id=1049">Air Traffic Chief</a> Control the busy skies and guide planes and helicopters to safe landing.</li>
+
+<li><a href="/play/?id=1050">Putt Putt Planet</a> Grab your putter and pick your color - it's time to play Mini Golf! It's as much fun as the amusement park, only online.</li>
+
+<li><a href="/play/?id=1053">Night of My Living Friends</a> The zombie horde has come for your friends! Only you can fight the undead and save your friends brains!</li>
+
+<li><a href="/play/?id=1055">Kings of Leon Pigeon Bomber</a> Rock out on bass and avoid the pigeon bombs to give the audience a great show!</li>
+
+<li><a href="/play/?id=1063">Quick Click</a> Leo won another award and he just won't shut up about it. Help Leo thank all his adoring fans!</li>
+
+<li><a href="/play/?id=1064">Fire 'em All!</a> With the election fast approaching, you're campaign budget is leaking fast and your supporters are not returing your calls. Can you reduce staff in time?</li>
+
+<li><a href="/play/?id=1065">Rally Driver</a> Don't be late for the interview! Skip traffic or bump and pound your way earning power ups - whatever it takes to rock the road!</li>
+
+<li><a href="/play/?id=1066">Leap of the Ninja</a> The Ninja is cool and crafty. With the grace of the crane and speed of the hawk, he leaps his way to ever higher goals. Help him leap his way to the top.</li>
+
+<li><a href="/play/?id=1067">Run Batman Run!</a> You are being chased by an angry mob. Don't let them catch you or you're done!</li>
+
+<li><a href="/play/?id=1069">Keep Alive</a> Can you keep the ball alive? Test your reflexes and prevent the ball from touching the ground.</li>
+
+<li><a href="/play/?id=1070">Closest To The Pin</a> Test your golf skills with the 7 iron. The pressure is on you, and you have only one swing. Can you get closest to the pin?</li>
+
+<li><a href="/play/?id=1071">Air MILF</a> The wolves are out of control and taking over the pristine Alaska wilderness. Can the govenor save the environment?</li>
+
+<li><a href="/play/?id=1072">I Love Toys</a> Hey! The 80's called and they want their toys back. A unique puzzle game where you spin and match toys from a bygone era.</li>
+
+<li><a href="/play/?id=1073">Volcano</a> Rotate the pieces of pipe to create a path for the lava to follow from entry to exit. Can you find the path to safety?</li>
+
+<li><a href="/play/?id=1074">Sudoku</a> The popular 9x9 numbers game. Arrange the numbers such that each number appears once in each row, column and 3x3 grid. A real brain teaser!</li>
+
+<li><a href="/play/?id=1075">Border Security</a> Awaken your inner Homeland Security and protect the borders! Prevent unwanted and unlikely peoples from crossing the border.</li>
+
+<li><a href="/play/?id=1076">Sound Box</a> Put on your best beats as you create the drum, bass, sound fx, and voice over tracks and share it with your friends.</li>
+
+<li><a href="/play/?id=1077">Poker Slots</a> A video poker game and slot machine in one game! Spin the reels, make good hands, get big payouts!</li>
+
+<li><a href="/play/?id=1078">Stranded!</a> You are stranded on a remote island and must find a way to signal for help. Use your ingenuity to solve this puzzle.</li>
+
+<li><a href="/play/?id=1079">Revenge of Arcade</a> You were once the boss in the arcades. Now the machines want their revenge! Play through video game history to beat the arcade.</li>
+
+<li><a href="/play/?id=1081">Hock-A-Loogie</a> The boys want to cause some trouble at school and have some fun on everyone.</li>
+
+<li><a href="/play/?id=1082">Holiday Activity</a> Play along with our holiday carolling bells activity. Happy Holidays!</li>
+
+<li><a href="/play/?id=1083">Match Master</a> You think you have a good memory? See how many levels you can master in our memory challenge to take the Match Master Crown!</li>
+
+<li><a href="/play/?id=1084">The Real Housewives Memory Challenge</a> You think you have a good memory? See how many levels you can master in our memory challenge to take the Match Master Crown!</li>
+
+<li><a href="/play/?id=1085">Top Chef Memory Challenge</a> You think you have a good memory? See how many levels you can master in our memory challenge to take the Top Chef memory crown!</li>
+
+<li><a href="/play/?id=1086">Hextris</a> Inspired by Tetris, rotate the Hexagon to catch falling blocks and prevent them from stacking too high.</li>
+
+<li><a href="/play/?id=1087">Titanic: Keys to the Past</a> Can you unlock the secrets of the world's most famous cruise liner?</li>
+
+<li><a href="/play/?id=1088">Fishy Rush</a> Help the little fish on its way through the ocean. Be careful: the deep sea is full of dangers!</li>
+
+<li><a href="/play/?id=1089">Rotor Storm</a> Pilot your helicopter through dangerous territory and eliminate the enemies in your path while avoiding their attacks.</li>
+
+<li><a href="/play/?id=1090">50 Shades of Trivia</a> Answer these trivia questions about the popular book 50 Shades of Grey.</li>
+
+<li><a href="/play/?id=1091">2048</a> You will need all your brain cells to achieve a high score. Join equal tiles: when equal numbers touch, they add up!</li>
+
+<li><a href="/play/?id=1092">Fit It Quick</a> A puzzle game for everyone who likes to give their brain a workout! Place Tetris-shaped blocks in the correct position.</li>
+
+<li><a href="/play/?id=1093">Candyland: Sweet River</a> Set sail for a mysterious island as this candy matching adventure continues.</li>
+
+<li><a href="/play/?id=1094">Cake Madness</a> Match 3 with tasty treats! Special combinations and power ups make this a delictible game.</li>
+
+<li><a href="/play/?id=1095">Fracture</a> A mind-bending geometric puzzle game: complete the shape without retracing any lines.</li>
+
+<li><a href="/play/?id=1096">The Esfarelante</a> Dig endlessly to find food. Survive longer to accumulate more points and get ahead of your competitors.</li>
+
+<li><a href="/play/?id=1097">Daily Horoscope</a> Do you want to know your future? Check your daily horoscope to get insight into your personality and your future.</li>
+
+<li><a href="/play/?id=1098">Memory Quotes Quiz</a> Can you name who originally said these quotes regarding memory?</li>
+
+<li><a href="/play/?id=1099">Jelly Slice</a> Fun and simple, slice the jelly so that each slice contains only one star!</li>
+
+<li><a href="/play/?id=1100">Two Dots</a> Join the quest of two brave dots traversing land and sea in this dot-connecting puzzle. Based on the free TwoDots app.</li>
+
+<li><a href="/play/?id=1101">Jewel Academy</a> Jewel Academy is a gem-filled, action packed match-3 game with a ton of levels to complete.</li>
+
+<li><a href="/play/?id=1105">Inteligencia</a> Can you achieve high intelligence?</li>
   </ul>
 </div>
 <div id="section-Blog">
