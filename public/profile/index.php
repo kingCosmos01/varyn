3de9a2bce5dc487b8b02dc0b6de20c8c2ca4e0ce
John Foster <john@varyn.com>
2020-12-05 17:54:46 -0800

    Change id to class to address #332

--- public/profile/index.php
+++ public/profile/index.php
@@ -806,7 +806,7 @@ include_once(VIEWS_ROOT . 'header.php');
                                 <label for="register_form_agreement">I agree to the <a href="/tos/" target="_popup">Terms of Use</a><span class="required-field">*</span></label><br/>
                                 <span><small>No</small>&nbsp;&nbsp;<input type="range" name="register_form_agreement" class="validation-slider" id="register_form_agreement" placeholder="Slide this all the way left to agree" tabindex="13" min="0" max="2" />&nbsp;&nbsp;<small>Yes</small></span>
                             </div>
-                            <div class="form-group"><input type="submit" value="Register" name="popupregister" id="registerButton" class="btn btn-success"/><span id="rememberme-container"><input type="checkbox" tabindex="4" checked="checked" name="register_form_rememberme" id="register_form_rememberme"><label for="register_form_rememberme">Remember Me</label></span></div>
+                            <div class="form-group"><input type="submit" value="Register" name="popupregister" id="registerButton" class="btn btn-success"/><span class="rememberme-container"><input type="checkbox" tabindex="4" checked="checked" name="register_form_rememberme" id="register_form_rememberme"><label for="register_form_rememberme">Remember Me</label></span></div>
 <?php
         } else {
 ?>
@@ -860,7 +860,7 @@ include_once(VIEWS_ROOT . 'header.php');
                         <label for="login_form_username">User name:</label><input type="text" id="login_form_username" name="login_form_username" tabindex="1" maxlength="20" class="popup-form-input" value="<?php echo($userName);?>" autocorrect="off" autocomplete="username"/><br/>
                         <label for="login_form_password">Password:</label><input type="password" id="login_form_password" name="login_form_password" tabindex="2" maxlength="20" class="popup-form-input" value="<?php echo($password);?>" /><br/>
                         <input type="button" class="btn btn-success" id="login-button" title="Login" value="Login >" tabindex="3" onclick="profilePage.loginValidation();" /><input type="text" name="login_form_email" class="popup-form-address-input" /><input type="hidden" name="all-clear" value="<?php echo($hackerVerification);?>" />
-                        <span id="rememberme-container"><input type="checkbox" tabindex="4" checked="checked" name="login_form_rememberme" id="login_form_rememberme"><label for="login_form_rememberme">Remember Me</label></span>
+                        <span class="rememberme-container"><input type="checkbox" tabindex="4" checked="checked" name="login_form_rememberme" id="login_form_rememberme"><label for="login_form_rememberme">Remember Me</label></span>
                         <a id="profile_forgot_password" href="#" tabindex="5" onclick="profilePage.forgotPassword();">Forgot password?</a><input type="hidden" name="action" value="login" />
                     </div>
                     <div class="text-center">
