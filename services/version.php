93344a5dafdf7a83736cc653bdd2a95a0ed74e18
John Foster <varyndev@gmail.com>
2022-04-16 14:43:20 -0700

    version up 2.3.8 deployed

--- services/version.php
+++ services/version.php
@@ -2,7 +2,7 @@
  * and lock the system if under maintenance.
  */
 if ( ! defined('VARYN_VERSION')) {
-    define('VARYN_VERSION', '2.3.6');
+    define('VARYN_VERSION', '2.3.8');
 }
 define('VARYN_ADMIN_LOCK', false);
 define('ADMIN_LOCK_MESSAGE', '<h3>Varyn is OFFLINE</h3><p>Varyn is currently offline, most probably due to server maintenance.</p><p>If you have an immediate need for service please contact Varyn support <a href="mailto:support@varyn.com">support@varyn.com</a>.</p>' );
