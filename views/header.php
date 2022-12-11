5b64fab519366783da94246d1555c564dda7a2bf
John Foster <varyndev@gmail.com>
2022-12-11 11:13:17 -0800

    set http referrer policy #340

--- views/header.php
+++ views/header.php
@@ -106,6 +106,7 @@ if ($page == 'play') {
     <meta http-equiv="expires" content="0">
     <meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT">
     <meta http-equiv="pragma" content="no-cache">
+    <meta name="referrer" content="strict-origin-when-cross-origin">
     <meta name="format-detection" content="telephone=no">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="apple-mobile-web-app-capable" content="yes">
